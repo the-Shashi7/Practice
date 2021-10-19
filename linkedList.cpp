0114e3a533302e42bed2cb3856580831290f5433
Shashi Bhushan <mahto71997@gmail.com>
2021-10-19 14:45:46 +0530

    linkedList Input complexity

--- linkedList.cpp
+++ linkedList.cpp
@@ -15,16 +15,20 @@ Node* takeInput(){
     int data;
     cin>>data;
     Node*head = NULL;
+    Node*tail = NULL;
     while (data!= -0){
         Node *newNode = new Node(data);
         if(head== NULL){
             head = newNode;
+            tail = newNode;
         }else{
-            Node*temp =head;
+            /* Node*temp =head;
             while (temp->next != NULL){
                 temp = temp->next;
             }    
-           temp->next = newNode;
+           temp->next = newNode; */
+           tail->next =newNode;
+           tail = tail->next;
         }  
         cin>>data;
     }
